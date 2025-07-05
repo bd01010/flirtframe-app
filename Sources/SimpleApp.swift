460611e2c55bb9dd23fe148f7779e8b12e1c76eb
bd01010 <joemack02@gmail.com>
2025-07-04 21:57:25 -0700

    Add simple IPA build approach using XcodeGen

---
+++ Sources/SimpleApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct SimpleFlirtFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("FlirtFrame - Loading...")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
